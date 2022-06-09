83fa0eb1ff20f5a5a895e598e650101bd4bc8305
root <root@HomeLPT-001.localdomain>
2022-06-09 13:30:51 +0300

    Done with Test

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+//saturdayFun
+function saturdayFun(activity='roller-skate'){
+    return (`This Saturday, I want to ${activity}!`)
+}
+const response = saturdayFun()
+console.log(response)
+
+//MondayWork
+function mondayWork(activity = 'go to the office') {
+    return (`This Monday, I will ${activity}.`)
+}
+const response2 = saturdayFun()
+console.log(response2)
+
+//wrapAdjective
+function wrapAdjective(asterisk='*'){
+    return function (adjective){
+        return `You are ${asterisk}${adjective}${asterisk}!`
+    }
+
+}
+const wrapper=recover()
+console.log(wrapper)
+
